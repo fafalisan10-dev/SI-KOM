bec49c78eabf7d1335d4ee69701f1cd27aac9477
fafalisanz <24082010270@student.upnjatim.ac.id>
2026-07-09 18:32:36 +0700

    fix: trust proxy railway untuk https asset loading

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,11 +11,13 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-     ->withMiddleware(function (Middleware $middleware) {
-       $middleware->alias([
-           'role' => \App\Http\Middleware\CheckRole::class,
-       ]);
-   })
+    ->withMiddleware(function (Middleware $middleware) {
+        $middleware->alias([
+            'role' => \App\Http\Middleware\CheckRole::class,
+        ]);
+
+        $middleware->trustProxies(at: '*');
+    })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
             fn (Request $request) => $request->is('api/*'),
